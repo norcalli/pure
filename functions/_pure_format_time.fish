f38095a1a6254f4062a7f8d6ea15912325af1493
Ashkan Kiani <ashkan@zephyr>
2019-01-23 13:31:39 -0800

    Bugfix

--- functions/_pure_format_time.fish
+++ functions/_pure_format_time.fish
@@ -6,6 +6,7 @@ function _pure_format_time \
     
     if test $milliseconds -lt 0; return $fail; end
 
+    set --local millis (math -s0 "$milliseconds % 1000")
     set --local seconds (math -s0 "$milliseconds / 1000 % 60")
     set --local minutes (math -s0 "$milliseconds / 60000 % 60")
     set --local hours (math -s0 "$milliseconds / 3600000 % 24")
